5f878e8cf297c902442595c752b5eb053eb11846
sloanse <steven.e.sloan@gmail.com>
2025-07-18 16:53:54 -0400

    word count function added

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+# Code Functionalities
+def get_word_count(filepath):
+    count = 0
+    with open(filepath) as f:
+        file_contents = f.read()
+        file_contents.split()
+        for word in file_contents.split():
+            count += 1
+        print(f"{count} words found in the document")
+        
+        
+
+def main():
+    get_word_count("books/frankenstein.txt")
+
+
+main()
